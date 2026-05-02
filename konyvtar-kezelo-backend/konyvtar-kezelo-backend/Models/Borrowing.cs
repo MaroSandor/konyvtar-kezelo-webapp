ef97ab6f271245b0e677f5e9c8c46b17dfedc0c6
Sandor Maro <marosandor2001@gmail.com>
2026-05-02 09:49:28 +0200

    Ignore Borrowing navigation props; rename DueDate
    Add System.Text.Json.Serialization and mark Reader and Book navigation properties with [JsonIgnore] (made nullable) to avoid serializing related entities / circular references. Rename Borrowing.DueTime to DueDate for clearer naming. Note: consumers and any EF migrations may need updates to account for the renamed property.

--- konyvtar-kezelo-backend/konyvtar-kezelo-backend/Models/Borrowing.cs
+++ konyvtar-kezelo-backend/konyvtar-kezelo-backend/Models/Borrowing.cs
@@ -1,18 +1,21 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace konyvtar_kezelo_backend.Models;
 
 public class Borrowing
 {
     public int Id { get; set; }
-    
+
     public int ReaderId { get; set; }
-    public Reader Reader { get; set; }
+    [JsonIgnore]
+    public Reader? Reader { get; set; }
     
     public int BookId { get; set; }
-    public Book Book { get; set; }
+    [JsonIgnore]
+    public Book? Book { get; set; }
     
     public DateTime BorrowDate { get; set; }
 
-    public DateTime DueTime { get; set; }
+    public DateTime DueDate { get; set; }
 }
